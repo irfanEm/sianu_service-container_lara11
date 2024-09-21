16a65088bc57a2a2889eb6506d4accbc4d6c80a0
irfanEm <imachmud97@gmail.com>
2024-09-21 10:21:12 +0700

    Zero Configuration - test

---
+++ app/Dependencys/Services.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Dependencys;
+
+class Services
+{
+    public function sayHello(): string
+    {
+        return "Hai, ini dari class services.";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,18 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\DependencyController;
-use App\Http\Controllers\EventPusherController;
-use App\Http\Controllers\FirewallController;
-use App\Http\Controllers\helloWorldController;
-use App\Http\Controllers\PhotoController;
-use App\Http\Controllers\ReportAggregatorController;
-use App\Http\Controllers\ReportAnalyzerController;
-use App\Http\Controllers\UploadController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\VideoController;
+use App\Dependencys\Services;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\VideoController;
+use App\Http\Controllers\UploadController;
+use App\Http\Controllers\FirewallController;
+use App\Http\Controllers\DependencyController;
+use App\Http\Controllers\helloWorldController;
+use App\Http\Controllers\EventPusherController;
+use App\Http\Controllers\ReportAnalyzerController;
+use App\Http\Controllers\ReportAggregatorController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -37,3 +38,8 @@ Route::get('/tag-variadic', [ReportAggregatorController::class, 'generateReports
 // test Dependencys
 Route::get('/test-dependencys', [helloWorldController::class, 'test']);
 Route::get('/sapa-nama', [DependencyController::class, 'sayHello']);
+
+// test Zero configuration
+Route::get('/zero-config', function(Services $services){
+    return $services->sayHello();
+});
